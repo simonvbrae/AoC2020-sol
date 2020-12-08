3830afec837d0198c27604b5664551a4d96e262a
sivbraec <simon.vanbraeckel@ugent.be>
2020-12-08 10:59:51 +0100

    Update Day8 solution

--- day8/day8.py
+++ day8/day8.py
@@ -1,19 +1,3 @@
-# importing the module
-import doctest
-
-# def solve_1(input):
-#     """
-#     >>> solve_1()
-#     BFFFBBFRRR: row 70, column 7, seat ID 567.
-#     >>> solve_1()
-#     FFFBBBFRRR: row 14, column 7, seat ID 119.
-#     >>> solve_1()
-#     BBFFBBFRLL: row 102, column 4, seat ID 820.
-#     """
-#     result=0
-#     return result
-
-
 def solve(linenr, cur_accval, accvals, lines, can_change_ins):
     if linenr == len(lines):
         print("Program terminated with accval")
